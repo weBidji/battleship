5dc8fe5e28ad10e1df31e54df0cdba46be68f2df
Bidji <159267099+weBidji@users.noreply.github.com>
2024-10-12 16:48:25 +0200

    add player creation

--- src/player.js
+++ src/player.js
@@ -1,3 +1,18 @@
-class Player {
-    // own gameboard, one player and one CPU, and eventually 2-player mode?
+import { Gameboard } from "./gameboard";
+import { Ship } from "./ship";
+
+export class Player {
+  // own gameboard, one player and one CPU, and eventually 2-player mode?
+  constructor() {
+    this.gameBoard = new Gameboard();
+    this.carrier = new Ship('carrier', 5);
+    this.battleship = new Ship('battleship', 4);
+    this.cruiser = new Ship('cruiser', 3);
+    this.submarine = new Ship('submarine', 3);
+    this.destroyer = new Ship('destroyer', 2);
   }
+}
+
+
+// ships to gameboard
+
